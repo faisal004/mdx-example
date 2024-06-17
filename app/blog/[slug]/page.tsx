88623f5a45dbf15b03fb3a5f520772f7e38e1d20
faisal004 <faisalhusain1320@gmail.com>
2024-06-17 14:58:20 +0530

    added : mdx component added

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { allBlogs } from '@/.contentlayer/generated'
+import { Mdx } from '../_components/mdx-component'
 
 export default async function Blog({ params }: { params: any }) {
-  console.log(allBlogs[0]._raw.flattenedPath)
   const blog = allBlogs.find(
     (blog) => blog._raw.flattenedPath === `blog/${params.slug}`,
   )
@@ -10,13 +10,16 @@ export default async function Blog({ params }: { params: any }) {
   }
 
   return (
-    <div className="flex flex-col  pt-28 text-white  bg-zinc-900 items-center justify-start mx-auto min-h-screen px-3  md:px-0">
-      <article className=" mb-10 max-w-[700px] ">
+    <div className="flex flex-col  pt-28 text-white  bg-zinc-900 items-start  justify-start mx-auto min-h-screen px-3  md:px-0">
+      <article className=" mb-10 max-w-[700px] p-10  ">
         <div className="flex flex-col gap-8 mx-1 md:mx-0   ">
           <div className="flex max-w-2xl flex-col gap-4 text-pretty  w-full">
             <h1 className="text-3xl font-bold leading-tight tracking-tight  text-left">
               {blog.title}
             </h1>
+          </div>
+          <div>
+            <Mdx code={blog.body.code} />
           </div>
         </div>
       </article>
---
+++ app/blog/_components/mdx-component.tsx
@@ -0,0 +1,20 @@
+
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+
+
+
+interface MdxProps {
+  code: string;
+}
+
+export function Mdx({ code }: MdxProps) {
+  const Component = useMDXComponent(code);
+
+  return (
+    <div className="mdx">
+        <Component />
+ 
+    </div>
+  );
+}
