aacd00fb5208f3dd138ac391534269b26c96a055
faisal004 <faisalhusain1320@gmail.com>
2024-06-17 14:30:48 +0530

    linted

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,21 +1,16 @@
-import { allBlogs } from "@/.contentlayer/generated"
-
-
-
+import { allBlogs } from '@/.contentlayer/generated'
 
 export default async function Blog({ params }: { params: any }) {
-    console.log(allBlogs[0]._raw.flattenedPath)
-    console.log(params.slug)
-  const blog = allBlogs.find((blog) => blog._raw.flattenedPath === `blog/${params.slug}`)
-console.log(blog)
+  console.log(allBlogs[0]._raw.flattenedPath)
+  const blog = allBlogs.find(
+    (blog) => blog._raw.flattenedPath === `blog/${params.slug}`,
+  )
   if (!blog) {
     return
   }
 
   return (
     <div className="flex flex-col  pt-28 text-white  bg-zinc-900 items-center justify-start mx-auto min-h-screen px-3  md:px-0">
-  
-      
       <article className=" mb-10 max-w-[700px] ">
         <div className="flex flex-col gap-8 mx-1 md:mx-0   ">
           <div className="flex max-w-2xl flex-col gap-4 text-pretty  w-full">
@@ -23,9 +18,7 @@ console.log(blog)
               {blog.title}
             </h1>
           </div>
-       
         </div>
-   
       </article>
     </div>
   )
