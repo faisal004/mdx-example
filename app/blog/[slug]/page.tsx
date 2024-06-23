230fd27cdef78e78f740d4a3b29b950697b582e2
faisal004 <faisalhusain1320@gmail.com>
2024-06-23 22:10:15 +0530

    fixed : css improved

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,15 +10,15 @@ export default async function Blog({ params }: { params: any }) {
   }
 
   return (
-    <div className="flex flex-col  pt-28 text-white  bg-zinc-900 items-start  justify-start mx-auto min-h-screen px-3  md:px-0">
-      <article className=" mb-10 max-w-[700px] p-10  ">
-        <div className="flex flex-col gap-8 mx-1 md:mx-0   ">
+    <div className="flex flex-col   text-white  items-start  justify-start mx-auto  px-3  md:px-0">
+      <article className=" mb-10 max-w-[700px] md:p-10 p-5 w-full mx-auto ">
+        <div className="flex flex-col gap-8 mx-1 md:mx-0  w-full  ">
           <div className="flex max-w-2xl flex-col gap-4 text-pretty  w-full">
             <h1 className="text-3xl font-bold leading-tight tracking-tight  text-left">
               {blog.title}
             </h1>
           </div>
-          <div className='prose prose-headings:text-white prose-p:text-white'>
+          <div className='prose prose-headings:text-white prose-p:text-white w-full '>
             <Mdx code={blog.body.code} />
           </div>
         </div>
